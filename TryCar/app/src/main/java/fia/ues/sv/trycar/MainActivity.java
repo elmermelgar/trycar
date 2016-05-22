6eb5ff9cd10bb09debc9eebcee0f79040d557aaf
LR12003 <daviddust21@gmail.com>
2016-05-21 23:40:02 -0600

    API OBD integrated

--- TryCar/app/src/main/java/fia/ues/sv/trycar/MainActivity.java
+++ TryCar/app/src/main/java/fia/ues/sv/trycar/MainActivity.java
@@ -2,14 +2,31 @@ package fia.ues.sv.trycar;
 
 //import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+
+import com.github.pires.obd.commands.ObdMultiCommand;
+import com.github.pires.obd.commands.fuel.AirFuelRatioCommand;
+
+import java.io.File;
 
 public class MainActivity extends Activity {
 
+
+
+
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
